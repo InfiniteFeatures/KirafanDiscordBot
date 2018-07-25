dca58d4c7e2f0d28f0cf6238ef6b591effb0622e
Laura <laura.lavigueur@outlook.com>
2018-07-25 17:07:53 -0400

    Working prototype

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-//Get discord
+//Dependencies
 const Discord = require('discord.js');
-
-//Make client
-const client = new Discord.Client();
+const Twitter = require('twitter');
 
 //Get config
 const config = require('./config.json');
@@ -13,127 +11,64 @@ const secret = require('./secret.json');
 // secret.token contains the discord bot token.
 // secret.twitter contains the twitter app oauth keys.
 
+//Variables
+let discordConnect = false;     //Check if connected to discord
+let twitChan;                   //The channel to post tweets in
+
+//Make client
+const client = new Discord.Client();
+
 //Client functions
 client.on("ready", () => {
-    // This event will run if the bot starts, and logs in, successfully.
-    console.log(`Bot has started, with ${client.users} users, in ${client.channels} channels of ${client.guilds} servers.`);
-    // Example of changing the bot's playing game to something useful. `client.user` is what the
-    // docs refer to as the "ClientUser".
-    client.user.setActivity(`Serving ${client.guilds.size} servers`);
-});
+    //When the bot logs in successfully.
+    discordConnect = true;
 
-client.on("guildCreate", guild => {
-    // This event triggers when the bot joins a guild.
-    console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-    client.user.setActivity(`Serving ${client.guilds.size} servers`);
-});
+    //Get the twitter channel (hardcoded)
+    twitChan = client.guilds.find("name", "TheTestServer").channels.find("name", "twitter");
 
-client.on("guildDelete", guild => {
-    // this event triggers when the bot is removed from a guild.
-    console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-    client.user.setActivity(`Serving ${client.guilds.size} servers`);
+    console.log(`Bot has started`);
 });
 
 client.on("message", async message => {
-    // This event will run on every single message received, from any channel or DM.
+    //Every single message received, from any channel or DM.
 
-    // It's good practice to ignore other bots. This also makes your bot ignore itself
-    // and not get into a spam loop (we call that "botception").
+    //Ignore messages from bots (including self)
     if(message.author.bot) return;
 
-    // Also good practice to ignore any message that does not start with our prefix,
-    // which is set in the configuration file.
+    //Ignore messages that don't start with the defined prefix
     if(message.content.indexOf(config.prefix) !== 0) return;
 
-    // Here we separate our "command" name, and our "arguments" for the command.
-    // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
-    // command = say
-    // args = ["Is", "this", "the", "real", "life?"]
+    //Split the command and arguments
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
-    // Let's go with a few common example commands! Feel free to delete or change those.
-
-    if(command === "ping") {
-        // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-        // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
-        const m = await message.channel.send("Ping?");
-        m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-    }
-
-    if(command === "say") {
-        // makes the bot say something and delete the message. As an example, it's open to anyone to use.
-        // To get the "message" itself we join the `args` back into a string with spaces:
-        const sayMessage = args.join(" ");
-        // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-        message.delete().catch(O_o=>{});
-        // And we get the bot to say the thing:
-        message.channel.send(sayMessage);
-    }
-
-    if(command === "kick") {
-        // This command must be limited to mods and admins. In this example we just hardcode the role names.
-        // Please read on Array.some() to understand this bit:
-        // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
-        if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
-            return message.reply("Sorry, you don't have permissions to use this!");
-
-        // Let's first check if we have a member and if we can kick them!
-        // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
-        // We can also support getting the member by ID, which would be args[0]
-        let member = message.mentions.members.first() || message.guild.members.get(args[0]);
-        if(!member)
-            return message.reply("Please mention a valid member of this server");
-        if(!member.kickable)
-            return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
-
-        // slice(1) removes the first part, which here should be the user mention or ID
-        // join(' ') takes all the various parts to make it a single string.
-        let reason = args.slice(1).join(' ');
-        if(!reason) reason = "No reason provided";
-
-        // Now, time for a swift kick in the nuts!
-        await member.kick(reason)
-            .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
-        message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+    //Commands here
+    if (command === "") {
 
     }
 
-    if(command === "ban") {
-        // Most of this command is identical to kick, except that here we'll only let admins do it.
-        // In the real world mods could ban too, but this is just an example, right? ;)
-        if(!message.member.roles.some(r=>["Administrator"].includes(r.name)) )
-            return message.reply("Sorry, you don't have permissions to use this!");
+});
 
-        let member = message.mentions.members.first();
-        if(!member)
-            return message.reply("Please mention a valid member of this server");
-        if(!member.bannable)
-            return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
+//Make Twitter
+const twitter = new Twitter(secret.twitter);
 
-        let reason = args.slice(1).join(' ');
-        if(!reason) reason = "No reason provided";
+//Start a stream following @kirarafantasia
+let stream = twitter.stream('statuses/filter', {follow: '856385582401966080'});
 
-        await member.ban(reason)
-            .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
-        message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
-    }
+stream.on('data', function(event) {
+    //Whenever the stream receives any kind of data (tweets/retweets by the target, replies to the target, retweets of the target, etc.)
 
-    if(command === "purge") {
-        // This command removes all messages from all users in the channel, up to 100.
-
-        // get the delete count, as an actual number.
-        const deleteCount = parseInt(args[0], 10);
-
-        // Ooooh nice, combined conditions. <3
-        if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-            return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-
-        // So we get our messages, and delete them. Simple enough, right?
-        const fetched = await message.channel.fetchMessages({limit: deleteCount});
-        message.channel.bulkDelete(fetched)
-            .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+    //Make sure we're only handling tweets/retweets by the target
+    if (event && event.text && event.user.id_str === '856385582401966080' && discordConnect) {
+        //Send to discord
+        twitChan.send(`${event.text}\n\nhttps://twitter.com/${event.user.screen_name}/status/${event.id_str}`);
     }
 });
 
-client.login(config.token);
+//Because errors are bad
+stream.on('error', function(error) {
+    console.log(error);
+});
+
+//Start the bot
+client.login(secret.token);
